71a605cdfd2369b477c31a000dbc23a849ca9676
subweeb <subeenregmi123@gmail.com>
2024-01-27 12:25:41 +0000

    worked on generic.c and docsgen

--- datastructure/generic.c
+++ datastructure/generic.c
@@ -1,16 +1,18 @@
 /* Copyright (C) 2024
  *
  * Author: Subeen Regmi <subeenregmi123@gmail.com>
+ * Date: 2024-01-27
  *
  * Implementation of a generic item.
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include "generic.h"
 
-void print_item(Item item) {
+void GI_print(GenericItem item) {
     switch (item.type) {
         case INTEGER:
             printf("%i", item.i);
@@ -33,7 +35,7 @@ void print_item(Item item) {
     }
 }
 
-void copy_item(Item *dest, Item *src) {
+void GI_copy(GenericItem *src, GenericItem *dest) {
     dest->type = src->type;
     switch (src->type) {
         case INTEGER:
@@ -54,7 +56,7 @@ void copy_item(Item *dest, Item *src) {
     }
 }
 
-bool compare(Item item1, Item item2) {
+bool GI_compare(GenericItem item1, GenericItem item2) {
     if (item1.type != item2.type) {
         return false;
     } 
@@ -62,52 +64,56 @@ bool compare(Item item1, Item item2) {
         case INTEGER:
             return item1.i == item2.i;
         case STRING:
-            if (strcmp(item1.s, item2.s) == 0){
-                return true;
-            }
-            return false;
+            return !strcmp(item1.s, item2.s);
         case CHAR:
             return item1.c == item2.c;
         case BOOL:
             return item1.b == item2.b;
+        default:
+            return false;
     }
 }
 
-void set_int(Item *item, int data) {
+inline void GI_set_int(GenericItem *item, int data) {
     item->type = INTEGER;
     item->i = data;
 }
 
-void set_string(Item *item, char data[]) {
+inline void GI_set_string(GenericItem *item, char data[]) {
     item->type = STRING;
     strcpy(item->s, data);
 }
 
-void set_char(Item *item, char data) {
+inline void GI_set_char(GenericItem *item, char data) {
     item->type = CHAR;
     item->c = data;
 }
 
-void set_bool(Item *item, bool data) {
+inline void GI_set_bool(GenericItem *item, bool data) {
     item->type = BOOL;
     item->b = data;
 }
 
-/*
-int main(){
-    bool b = !5;
-    printf("%i\n", b);
-    Item i1 = {{1}, INTEGER};
-    Item i2 = {{2}, INTEGER};
-    Item i3 = {{1}, INTEGER};
-
-    char str[] = "subeen";
-    Item i4 = {{.s = str}, STRING};
-    Item i5 = {{.s = "pop"}, STRING};
-    Item i6 = {{.s = "subeen"}, STRING};
-
-    printf("%i, %i\n", compare(i1, i2), compare(i3, i1));
-
-    printf("%i, %i\n", compare(i4, i5), compare(i4, i6));
+GenericItem GI_create_int(int data) {
+    GenericItem item;
+    GI_set_int(&item, data);
+    return item;
 }
-*/
+
+GenericItem GI_create_string(char data[]) {
+    GenericItem item;
+    GI_set_string(&item, data);
+    return item;
+}
+
+GenericItem GI_create_char(char data) {
+    GenericItem item;
+    GI_set_char(&item, data);
+    return item;
+}
+
+GenericItem GI_create_bool(bool data) {
+    GenericItem item;
+    GI_set_bool(&item, data);
+    return item;
+}
--- datastructure/generic.h
+++ datastructure/generic.h
@@ -1,41 +1,54 @@
 /* Copyright (C) 2024
  *
  * Author: Subeen Regmi <subeenregmi123@gmail.com>
+ * Date: 2024-01-27
  *
  * Implementation of a generic item.
  */
+
+// #DOCSGEN-START
 
 #ifndef DATASTRUCTURE_GENERIC_H
 #define DATASTRUCTURE_GENERIC_H
 
 #include <stdbool.h>
 
-#define MAX_STRING_LENGTH 64
+#define MAX_STRING_LENGTH 1024
 
-typedef enum Types {
+
+// Enum for all types supported the 'generic' type.
+typedef enum GenericTypes {
     INTEGER,
     STRING,
     CHAR,
     BOOL
-} Types;
+} GenericTypes;
 
-typedef struct Item {
+// Representation of a generic item.
+typedef struct GenericItem {
     union {
         int i;
         char s[MAX_STRING_LENGTH];
         char c;
         bool b;
     };
-    Types type;
-} Item;
+    GenericTypes type;
+} GenericItem;
 
-void print_item(Item item);
-void copy_item(Item *dest, Item *src);
-bool compare(Item item1, Item item2);
+void GI_print(GenericItem item);
+void GI_copy(GenericItem *src, GenericItem *dest);
+bool GI_compare(GenericItem item1, GenericItem item2);
 
-void set_int(Item *item, int data);
-void set_string(Item *item, char data[]);
-void set_char(Item *item, char data);
-void set_bool(Item *item, bool data);
+void GI_set_int(GenericItem *item, int data);
+void GI_set_string(GenericItem *item, char data[]);
+void GI_set_char(GenericItem *item, char data);
+void GI_set_bool(GenericItem *item, bool data);
 
-#endif /* DATASTRUCTURE_GENERIC_H */
+GenericItem GI_create_int(int data);
+GenericItem GI_create_string(char data[]);
+GenericItem GI_create_char(char data);
+GenericItem GI_create_bool(bool data);
+
+#endif // DATASTRUCTURE_GENERIC_H 
+
+// #DOCSGEN-END
